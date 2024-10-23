8b4239a3c9a8fa29bd4e38ca481f95d4ca621109
Akseli Kiviruusu <akselikiviruusu@gmail.com>
2024-10-23 13:58:01 +0300

    tehtävä 11_1

---
+++ moduuli11/tehtävä11_1.py
@@ -0,0 +1,29 @@
+class Julkaisu:
+    def __init__(self, nimi):
+        self.nimi = nimi
+
+    def tulosta_tiedot(self):
+        print(self.nimi)
+
+class Lehti(Julkaisu):
+    def __init__(self, nimi, paatoimittaja):
+        self.paatoimittaja = paatoimittaja
+        super().__init__(nimi)
+
+    def tulosta_tiedot(self):
+        super().tulosta_tiedot()
+        print(self.paatoimittaja)
+
+class Kirja(Julkaisu):
+    def __init__(self,nimi, kirjoittaja, sivumaara):
+        self.kirjoittaja = kirjoittaja
+        self.sivumaara = sivumaara
+        super().__init__(nimi)
+
+    def tulosta_tiedot(self):
+        super().tulosta_tiedot()
+        print(self.kirjoittaja)
+        print(self.sivumaara)
+
+julkaisu = Kirja('Harry Potter', 'J.K. Rowling', 459)
+julkaisu.tulosta_tiedot()
